06b1fc63ac9627d1277c78f8a9ef60b082a27193
Leonardo Riether <leonardoalves8217@gmail.com>
2016-02-25 20:37:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ var q = doc.querySelector.bind(doc);
 var eidx;
 eidx = Math.floor(Math.random()*Elements.length);
 
-var elem = q('.elem');
-elem.innerHTML = Elements[eidx].Key;
+var 
+  elem = q('.elem'),
+  ekey = q('.elem .key'),
+  enumb = q ('.elem .numb')
+;
+ekey.innerHTML = Elements[eidx].Key;
+enumb.innerHTML = eidx;
 
 })(window, document)

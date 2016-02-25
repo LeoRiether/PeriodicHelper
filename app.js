f8e27f89ff139bf374732148424fac8cf448c91e
Leonardo Riether <leonardoalves8217@gmail.com>
2016-02-25 19:44:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+(function (win, doc){
+'use strict';
 
+var q = doc.querySelector.bind(doc);
+
+var eidx;
+eidx = Math.floor(Math.random(Elements.length));
+
+var elem = q('.elem');
+elem.innerHTML = Elements[eidx].Key;
+
+})(window, document)

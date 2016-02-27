36a5d8f22c23c84e428135b1d109f40b99ec88a1
Leonardo Riether <leonardoalves8217@gmail.com>
2016-02-27 12:01:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,42 @@
 var q = doc.querySelector.bind(doc);
 
 var eidx;
-eidx = Math.floor(Math.random()*Elements.length);
 
 var 
   elem = q('.elem'),
   ekey = q('.elem .key'),
-  enumb = q ('.elem .numb')
+  enumb = q ('.elem .numb'),
+  answtxt = q('#answ'),
+  answbtn = q('.answbtn'),
+  msgbox = q('#msgbox'),
+  msg = q('#msgbox .content')
 ;
-ekey.innerHTML = Elements[eidx].Key;
-enumb.innerHTML = eidx;
+
+function validate(){
+  if(answtxt.value == Elements[eidx].Name) {
+    msgbox.className = "ok";
+    msg.innerHTML = "Thats correct!";
+  } else {
+    msgbox.className = "bad";
+    msg.innerHTML = "Incorrect... The answer was <b>" + Elements[eidx].Name + "</b>";
+  }
+}
+
+function next() {
+  eidx = Math.floor(Math.random()*Elements.length);
+  ekey.innerHTML = Elements[eidx].Key;
+  enumb.innerHTML = eidx;
+}
+
+q('#msgbox .close').addEventListener("click", function (){
+  msgbox.className = "";
+  next();
+});
+
+answbtn.addEventListener("click", validate);
+answtext.addEventListener("keyup", function (e){
+  if(e.which == 13) // Enter
+    validate();
+})
 
 })(window, document)

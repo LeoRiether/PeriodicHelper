afad3b550f6018421c662c3a9ce37bddef317278
Leonardo Riether <leonardoalves8217@gmail.com>
2016-03-24 12:29:14 -0300

    Create localize.js

---
+++ localize.js
@@ -0,0 +1,39 @@
+(function (doc, $$) {
+'use strict';
+
+var strings = [
+  title: {
+    en: 'Periodic Helper',
+    pt: 'Tabela Periódica'
+  },
+  desc: {
+    en: 'Learn the periodic table in no time!',
+    pt: 'Aprenda a tabela periódica em pouco tempo!'
+  },
+  by: {
+    en: 'Created by: Leonardo Riether',
+    pt: 'Criado por Leonardo Riether'
+  },
+  gitproj: {
+    en: 'This project on github',
+    pt: 'Este projeto no github'
+  }
+];
+
+var lang = (function () {
+  var l = localStorage.getItem('lang');
+  if (l !== null) { return l; }
+  else {
+    localStorage.setItem('lang', 'en');
+    return 'en';
+  }
+})();
+
+$$('[string]').forEach(function (e) {
+  e.textContent = strings[e.attributes.string][lang];
+});
+
+window.strings = strings;
+window.lang = lang;
+
+})(document, document.querySelectorAll.bind(document));

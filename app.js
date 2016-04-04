aef1449b9145ab3ab16b02ceed680215cfe69603
Leonardo Riether <leonardoalves8217@gmail.com>
2016-04-04 16:06:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -48,8 +48,28 @@ langsel.addEventListener('change', function () {
   window.localize();
 });
 
+function remAcentos( newStringComAcento ) {
+  var string = newStringComAcento;
+    var mapaAcentosHex     = {
+        a : /[\xE0-\xE6]/g,
+        e : /[\xE8-\xEB]/g,
+        i : /[\xEC-\xEF]/g,
+        o : /[\xF2-\xF6]/g,
+        u : /[\xF9-\xFC]/g,
+        c : /\xE7/g,
+        n : /\xF1/g
+    };
+
+    for ( var letra in mapaAcentosHex ) {
+        var expressaoRegular = mapaAcentosHex[letra];
+        string = string.replace( expressaoRegular, letra );
+    }
+
+    return string;
+}
+
 function validate(){
-  if(answtxt.value.toLowerCase() == Elements[eidx]['Name-'+window.lang()].toLowerCase()) {
+  if(remAcentos(answtxt.value).toLowerCase().trim() == remAcentos(Elements[eidx]['Name-'+window.lang()]).toLowerCase().trim()) {
     msgbox.className = "ok";
     msg.innerHTML = msgLocal[window.lang()][0];
     lc = true;
